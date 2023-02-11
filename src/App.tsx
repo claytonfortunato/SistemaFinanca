cf4ac0b619f105814274250f4d086354157471b5
claytonfortunato <claytonreis10@gmail.com>
2023-02-11 16:29:09 -0300

    feat:  header da tabela

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import { useState, useEffect } from "react";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { filterListByMonth, getCurrentMonth } from "./helpers/dateFilter";
+import { TableArea } from "./components/TableArea";
+
 import * as C from "./App.styles";
 
 function App() {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
   return (
     <div className="App">
-      <C.Container>AOBA!</C.Container>
+      <C.Container>
+        <C.Header>
+          <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          {/* Área de informações */}
+
+          {/* Área de inserção */}
+
+          <TableArea />
+        </C.Body>
+      </C.Container>
     </div>
   );
 }
