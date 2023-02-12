0f6041009c821066c74b97323be279ead35a611b
claytonfortunato <claytonreis10@gmail.com>
2023-02-11 23:03:39 -0300

    feat: projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
+import * as C from "./App.styles";
 import { Item } from "./types/Item";
-import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { items } from "./data/items";
-import { filterListByMonth, getCurrentMonth } from "./helpers/dateFilter";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
 import { TableArea } from "./components/TableArea";
 import { InfoArea } from "./components/InfoArea";
+import { InputArea } from "./components/InputArea";
 
-import * as C from "./App.styles";
-
-function App() {
+const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
@@ -40,27 +39,31 @@ function App() {
     setCurrentMonth(newMonth);
   };
 
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList);
+  };
+
   return (
-    <div className="App">
-      <C.Container>
-        <C.Header>
-          <C.HeaderText>Sistema Financeiro</C.HeaderText>
-        </C.Header>
-        <C.Body>
-          <InfoArea
-            currentMonth={currentMonth}
-            onMonthChange={handleMonthChange}
-            income={income}
-            expense={expense}
-          />
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
 
-          {/* Área de inserção */}
+        <InputArea onAdd={handleAddItem} />
 
-          <TableArea list={filteredList} />
-        </C.Body>
-      </C.Container>
-    </div>
+        <TableArea list={filteredList} />
+      </C.Body>
+    </C.Container>
   );
-}
+};
 
 export default App;
