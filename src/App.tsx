9293539f9da1908f74fc4d7a94c2092b912746f6
claytonfortunato <claytonreis10@gmail.com>
2023-02-11 22:30:48 -0300

    feat: area de dispesas

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,28 @@ function App() {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList]);
 
   const handleMonthChange = (newMonth: string) => {
     setCurrentMonth(newMonth);
@@ -32,11 +50,13 @@ function App() {
           <InfoArea
             currentMonth={currentMonth}
             onMonthChange={handleMonthChange}
+            income={income}
+            expense={expense}
           />
 
           {/* Área de inserção */}
 
-          <TableArea list={list} />
+          <TableArea list={filteredList} />
         </C.Body>
       </C.Container>
     </div>
